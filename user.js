7ff8dbe66bd8c52204f8489f2d5675a5e2dceb6a
Alireza Aslani1 <alirezaaslane.mob7@gmail.com>
2025-08-02 15:08:45 +0330

    feat(user class)

---
+++ user.js
@@ -0,0 +1,6 @@
+"use strict";
+class User {
+    constructor() {
+        this.name = "ali";
+    }
+}
---
+++ user.ts
@@ -0,0 +1,6 @@
+class User {
+  name: string;
+  constructor() {
+    this.name = "ali";
+  }
+}
